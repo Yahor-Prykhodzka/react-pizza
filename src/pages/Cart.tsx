1dc8b9c38a5eb83300c1dc27172efe2415a8f7ed
Yahor-Prykhodzka <yahorprykhodzka@gmail.com>
2024-06-21 15:09:45 +0200

    Updating styles of components across different devices

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,8 +4,9 @@ import InfoBlock from '../components/InfoBlock/InfoBlock'
 const Cart = () => {
   return (
     <>
-        <InfoBlock emoji={"🛒"} title={"The cart is empty!"} description={"Looks like you haven't gotten your order together"} />
+        <InfoBlock emoji={"🛒"} title={"The cart is empty!"} description={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis est rem natus non deserunt a."} />
     </>
+    
   )
 }
 
